b5e9c91f89f58655a6c1bc20082c4c2d3dfd6dae
kgbuyna <kgbuyna692@gmail.com>
2024-01-29 12:40:10 +0800

    .

--- app.config.js
+++
@@ -1,7 +0,0 @@
-const isProduction = process.env.NODE_ENV === 'production';
-export default {
-  extra: {
-    API_KEY: isProduction ? 'production_api_key' : 'AIzaSyCfoOZmSBlNKkgaXAu_kuH2R3O7r17PCyc',
-    ENVIRONMENT: isProduction ? 'production' : 'development',
-  },
-};
