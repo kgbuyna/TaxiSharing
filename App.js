545e2e7a255a379805242e900113994f68174db0
kgbuyna <kgbuyna692@gmail.com>
2024-03-10 06:45:32 +0800

    .

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// NODE_ENV=development npx expo start --tunnel
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import SplashScreen from "./src/screens/SplashScreen";
+import MainScreen from "./src/screens/MainScreen";
+import SearchScreen from "./src/screens/SearchScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import PostScreen from "./src/screens/PostScreen";
+import StartScreen from "./src/screens/StartScreen";
+import { Provider } from "react-redux";
+import store from "./store";
+import config from "./app.config"; // or import config from './app.config.development';
+import MessengerScreen from "./src/screens/MessengerScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import ChatListScreen from "./src/screens/ChatListScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+// config();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">   
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Start"
+            component={StartScreen}
+            options={{ headerShown: false }}
+          />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Post"
+            component={PostScreen}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Messenger"
+            component={MessengerScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={ChatScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ChatList"
+            component={ChatListScreen}
+            options={{ headerShown: false }}
+          />
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
