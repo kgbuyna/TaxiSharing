a6dc43e03cf53f94b15b4dac9333d95a3f2943fc
kgbuyna <kgbuyna692@gmail.com>
2024-04-26 15:34:22 +0800

    store

--- store.js
+++ store.js
@@ -1,12 +1,22 @@
 // app/store.js
 
-import { configureStore } from '@reduxjs/toolkit';
-import currentLocationReducer  from './slices/currentLocationSlice';
-import destinationLocationReducer from './slices/destinationLocationSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import currentLocationReducer from "./slices/currentLocationSlice";
+import destinationLocationReducer from "./slices/destinationLocationSlice";
+import userReducer from "./slices/userSlice";
+import tripReducer from "./slices/tripSlice";
+import messageReducer from "./slices/messageSlice";
 const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+
   reducer: {
     currentLocation: currentLocationReducer,
     destinationLocation: destinationLocationReducer,
+    user: userReducer,
+    trip: tripReducer,
+    message: messageReducer,
     // Add more reducers as needed
   },
 });
